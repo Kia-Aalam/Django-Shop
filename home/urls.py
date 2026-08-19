5023474465859488928b51f4b2f7c77a3fa9b1cc
Kia Aalam <kiaaalam7@gmail.com>
2026-08-19 22:52:04 +0330

    upgrade function to class in views(home app)

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 '''from django.conf import settings
 from django.conf.urls.static import static'''
+from home.views import index, ContactView
 
 urlpatterns = [
-    path('', views.index, name='home'), 
-    path('contact/', views.contact, name='contact')
+    path('', index.as_view(), name='home'), 
+    path('contact/', ContactView.as_view(), name='contact'),
     
 ]
 '''if settings.DEBUG:
--- home/views.py
+++ home/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render, redirect
-from .forms import ContactForm
-from .models import ContactModel
+from home.forms import ContactForm
+from home.models import ContactModel
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
-def index(request):
-    return render(request, 'index.html')
+# index page
+class index(TemplateView):
+    template_name = "index.html"
 
-def contact(request):
-        
-    if request.method == "POST":
-            form = ContactForm(request.POST)
-                
-            if form.is_valid():
-                ContactModel.objects.create(
-                    email=form.cleaned_data['email'],
-                    subject=form.cleaned_data['subject'],
-                    message=form.cleaned_data['message']
-                )   
-                return redirect('home')
-    else:
-        form = ContactForm()
-        
-    return render(request, 'contact.html', {'form':form})
+# contact page
+class ContactView(FormView):
+    template_name = "contact.html"
+    form_class = ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        ContactModel.objects.create(
+            email=form.cleaned_data['email'],
+            subject=form.cleaned_data['subject'],
+            message=form.cleaned_data['message']
+        )   
+        return super().form_valid(form)
