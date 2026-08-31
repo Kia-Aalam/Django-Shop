307c8a7459e5fda59423e36bafdd58705a6184d4
Kia Aalam <kiaaalam7@gmail.com>
2026-08-31 21:13:10 +0330

    change is_active to is_paid

--- cart/models.py
+++ cart/models.py
@@ -8,7 +8,7 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='carts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Cart {self.id} - {self.user.email}"
